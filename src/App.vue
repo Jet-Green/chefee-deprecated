b0d5848f7816e4f06b88857dac9ef30f272c84df
Grisha Dzyin <grishadzyin@gmail.com>
2022-07-03 16:36:41 +0400

    add icons

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,31 @@ onMounted(() => {
 
 <template>
   <router-view />
+  <a-layout-footer
+    :style="{ display: appState.xs ? 'block' : 'none', position: 'fixed', bottom: 0, zIndex: 1, width: '100%' }">
+    <a-row style="display: flex; justify-content: space-between" class="mr-16 ml-16">
+      <a-col>
+        <span class="mdi mdi-home-outline"></span>
+      </a-col>
+      <a-col>
+        <span class="mdi mdi-plus-circle-outline"></span>
+      </a-col>
+      <a-col>
+        <span class="mdi mdi-image-outline"></span>
+      </a-col>
+      <a-col>
+        <span class="mdi mdi-forum-outline"></span>
+      </a-col>
+      <a-col>
+        <span class="mdi mdi-account-outline"></span>
+      </a-col>
+    </a-row>
+  </a-layout-footer>
 </template>
 
-<style>
+<style scoped>
+footer {
+  padding: 0 !important;
+  font-size: 33px;
+}
 </style>
