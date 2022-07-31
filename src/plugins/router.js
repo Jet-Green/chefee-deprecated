3282b79560f7de3739b87c2fd754b5555b1ed68d
Grisha Dzyin <grishadzyin@gmail.com>
2022-07-31 19:04:17 +0400

    add camera

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomePage from '../views/WelcomePage.vue'
+import Camera from '../views/Camera.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,10 @@ const router = createRouter({
             path: '/search',
             name: 'SearchPage',
             component: () => import('../views/SearchPage.vue'),
+        }, {
+            path: '/camera',
+            name: 'Camera',
+            component: Camera,
         },
     ]
 })
---
+++ src/views/Camera.vue
@@ -0,0 +1,5 @@
+<script setup>
+</script>
+<template>
+    <input type="file" accept="image/*" capture="camera" />
+</template>
